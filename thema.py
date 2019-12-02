80662f4b4854a918b3f309d5ae122efaf204e005
saturdayPizza <mamonox@gmail.com>
2019-12-03 01:41:09 +0900

    csv出力処理を追加

--- thema.py
+++ thema.py
@@ -1,27 +1,35 @@
 # 匿名ラジオの動画タイトルを集めるプログラム
 # リンク : https://www.youtube.com/channel/UClSsb_e0HDQ-w7XuwNPgGqQ
-# 集めた結果は"tyema.csv"に保存する
+# 集めた結果は"thema.csv"に保存する
 import requests
 from bs4 import BeautifulSoup
 import sys
+import csv
 
 r = requests.get("https://www.youtube.com/channel/UClSsb_e0HDQ-w7XuwNPgGqQ/videos")
 q = requests.get("https://www.youtube.com/channel/UClSsb_e0HDQ-w7XuwNPgGqQ/videos?view=0&sort=da&flow=grid")
 
 #print(r.text)
-print(q.text)
+#print(q.text)
 
-soup = BeautifulSoup(r.content, "html.parser")
+#soup = BeautifulSoup(r.content, "html.parser")
 soup_1 = BeautifulSoup(q.content, "html.parser")
 
 real_page_tags = soup_1.find_all("h3")
 for tag in real_page_tags:
     print(tag.text)
 
-sys.exit()
-
+with open('thema.csv', 'w') as f:
+    writer = csv.writer(f, lineterminator='\n') 
+    list = []
+    for tag in real_page_tags:
+        list.append(tag.text)
+    writer.writerow(list)
+#sys.exit()
+'''
 real_page_tags = soup.find_all("h3")
 for tag in real_page_tags:
-    print(tag.text)
+    #print(tag.text)
 
-#print(soup.find_all("h3", "yt-lockup"))
+print(soup.find_all("h3", "yt-lockup"))
+'''
